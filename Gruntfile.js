08374d19c93b7d53a5df905bca05585d77ad6b8e
maiordom <maiordom@yandex.ru>
2013-06-10 18:31:02 +1100

    upd

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,56 +1,62 @@
 module.exports = function( grunt ) {
     grunt.initConfig({
         concat: {
-            js: {
-                sprite: {
-                    src: [
-                        'js/src/sprite.canvas.js',
-                        'js/src/sprite.document.js',
-                        'js/src/sprite.resize.js',
-                        'js/src/sprite.canvas-element.js',
-                        'js/src/sprite.global.js',
-                        'js/src/sprite.css-element.js',
-                        'js/src/sprite.css-panel.js',
-                        'js/src/sprite.test.js',
-                        'js/src/sprite.js'
-                    ],
-                    dest: 'bundle/sprite.all.js'
-                },
-                libs: {
-                    src: [
-                        'js/libs/jquery-1.8.3.js',
-                        'js/libs/underscore-1.4.4.js',
-                        'js/libs/backbone-1.0.0.js',
-                        'js/libs/scrollpane/jquery.jscrollpane.js',
-                        'js/libs/scrollpane/jquery.mousewheel.js'
-                    ],
-                    dest: 'bundle/libs.all.js'
-                }
+            js_sprite: {
+                src: [
+                    'client/js/src/sprite.canvas.js',
+                    'client/js/src/sprite.document.js',
+                    'client/js/src/sprite.resize.js',
+                    'client/js/src/sprite.canvas-element.js',
+                    'client/js/src/sprite.global.js',
+                    'client/js/src/sprite.css-element.js',
+                    'client/js/src/sprite.css-panel.js',
+                    'client/js/src/sprite.test.js',
+                    'client/js/src/sprite.js'
+                ],
+                dest: 'client/bundle/sprite.all.js'
+            }, 
+            js_libs: {
+                src: [
+                    'client/js/libs/jquery-1.8.3.js',
+                    'client/js/libs/underscore-1.4.4.js',
+                    'client/js/libs/backbone-1.0.0.js',
+                    'client/js/libs/scrollpane/jquery.jscrollpane.js',
+                    'client/js/libs/scrollpane/jquery.mousewheel.js'
+                ],
+                dest: 'client/bundle/libs.all.js'
             },
             css: {
                 src: [
-                    'css/fontello.css',
-                    'css/jquery.jscrollpane.css',
-                    'css/sprite.css'
+                    'client/css/fontello.css',
+                    'client/css/jquery.jscrollpane.css',
+                    'client/css/sprite.css'
                 ],
-                dest: 'bundle/sprite.all.css'
+                dest: 'client/bundle/sprite.all.css'
             }
         },
 
         uglify: {
-            js: {
-                src: '<%= concat.js.dest %>',
-                dest: 'bundle/sprite.min.js'
+            js_sprite: {
+                src: '<%= concat.js_sprite.dest %>',
+                dest: 'client/bundle/sprite.min.js'
             },
-            css: {
+            js_libs: {
+                src: '<%= concat.js_libs.dest %>',
+                dest: 'client/bundle/libs.min.js'
+            }
+        },
+
+        cssmin: {
+            minify: {
                 src: '<%= concat.css.dest %>',
-                dest: 'bundle/sprite.min.css'
+                dest: 'client/bundle/sprite.min.css'
             }
         }
     });
 
     grunt.loadNpmTasks( 'grunt-contrib-concat' );
     grunt.loadNpmTasks( 'grunt-contrib-uglify' );
+    grunt.loadNpmTasks( 'grunt-contrib-cssmin' );
 
-    grunt.registerTask( 'default', [ 'concat', 'uglify' ] );
+    grunt.registerTask( 'default', [ 'concat', 'uglify', 'cssmin' ] );
 };
